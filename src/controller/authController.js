dfa6477f683f326afe369edfb109364eef7e65e0
Vansovski <marcelo_vansovski@hotmail.com>
2022-06-22 18:57:47 -0300

    TokenGenerate

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const { route } = require("express/lib/application");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const auth =require('../config/auth.json');
 
 const User = require("../Model/Usuario");
 
 const router = express.Router();
+
+//Gerador de token
+function generatorToken(params = {})
+{
+    return jwt.sign(params,auth.secret,{
+        expiresIn:86400,
+    });
+}
 
 //Criação de Usuario
 router.post("/register", async (req, resp) => {
@@ -27,9 +37,9 @@ router.post("/register", async (req, resp) => {
     //Cria o usuario no banco de dados
     const user = await User.create(req.body);
     //Remove senha do retorno
-    //user.senha = undefined;
+    user.senha = undefined;
     //Retorno da API
-    return resp.send({ user });
+    return resp.send({ user, token: generatorToken({id:user.id}) });
   } catch (error) {
     //Falha de Cadastro 
     console.log(error);
@@ -38,9 +48,22 @@ router.post("/register", async (req, resp) => {
 });
 
 
+//Rota de autenticação
+router.post('/authenticate', async(req, resp) =>{
+    const{email, senha} = req.body;
+    //Verifica se já existe cadastro 
+    const userAlredy = await User.findOne({ where: { email: email } });
+    if(!userAlredy || !await bcrypt.compare(senha, userAlredy.senha))
+    {
+        return resp.status(400).send('Corrija senha e/ou email!');
+    }
 
+    //Remove senha
+    userAlredy.senha = undefined;
 
+    resp.send({userAlredy,token: generatorToken({id:userAlredy.id})});
 
+});
 
 
 module.exports = (app) => app.use("/auth", router);
